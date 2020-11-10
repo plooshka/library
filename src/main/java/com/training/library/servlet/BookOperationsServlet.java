99a182a214429bddb576a959f8cbb90aa30c75b0
Vlad <ovchinnikovlad@gmail.com>
2020-11-10 22:55:20 +0300

    idea-fix

--- src/main/java/com/training/library/servlet/BookOperationsServlet.java
+++ src/main/java/com/training/library/servlet/BookOperationsServlet.java
@@ -1,6 +1,5 @@
 package com.training.library.servlet;
 
-import com.sun.tools.sjavac.server.RequestHandler;
 import com.training.library.entity.Book;
 import com.training.library.service.DatabaseService;
 
@@ -11,18 +10,54 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookOperationsServlet extends HttpServlet {
     DatabaseService databaseService = new DatabaseService();
     PrintWriter writer = null;
-    //ADDBOOKSERVLET
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/create.jsp").forward(request, response);
+        String currentPath = request.getContextPath();
+        //index
+        //if (currentPath == "/demo_war") {
+            PrintWriter printWriter = response.getWriter();
+            List<Book> books = new ArrayList<Book>();
+            try (Connection conn = databaseService.setConnection()) {
+                books = databaseService.selectAll(conn);
+            } catch (Exception exception) {
+                printWriter.println("Failed to display the content");
+            }
+            request.setAttribute("books", books);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+       // }
+        //add
+       // if((currentPath + "add") == "/demo_war/add") {
+            getServletContext().getRequestDispatcher("/create.jsp").forward(request, response);
+        //}
+        //edit
+       // if((currentPath + "/edit") == "/demo_war/edit" ) {
+            writer = response.getWriter();
+            try {
+                Long id = Long.parseLong(request.getParameter("id"));
+                Book book = databaseService.selectById(id, databaseService.setConnection());
+                if (book != null) {
+                    request.setAttribute("book", book);
+                    getServletContext().getRequestDispatcher("/edit.jsp").forward(request, response);
+                } else {
+                    getServletContext().getRequestDispatcher("/index.jsp").forward(null, null);
+                }
+            } catch (Exception ex) {
+                writer.println("Failed to get data");
+            }
+       // }
+
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         writer = response.getWriter();
+        //add
         try(Connection conn = databaseService.setConnection()) {
             String bookName = request.getParameter("bookname");
             String author = request.getParameter("author");
@@ -33,17 +68,28 @@ public class BookOperationsServlet extends HttpServlet {
         catch(Exception ex) {
             writer.println("Failed to add a book. Try again");
         }
-    }
-
-    //DELETEBOOKSERVLET
-    /*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter printWriter = response.getWriter();
+        //delete
+        writer = response.getWriter();
         Long id = Long.parseLong(request.getParameter("id"));
         try(Connection conn = databaseService.setConnection()) {
             databaseService.delete(id, conn);
             response.sendRedirect(request.getContextPath() + "/");
         } catch (Exception ex) {
-            printWriter.println("Failed to delete a book.");
+            writer.println("Failed to delete a book.");
         }
-    }*/
+        //edit
+        try {
+            Long id1 = Long.parseLong(request.getParameter("id"));
+            String bookName = request.getParameter("bookname");
+            String author = request.getParameter("author");
+            Book book = new Book(id1, bookName, author);
+            databaseService.update(book, databaseService.setConnection());
+            response.sendRedirect(request.getContextPath() + "/");
+        }
+        catch(Exception ex) {
+            getServletContext().getRequestDispatcher("/notfound.html").forward(request, response);
+        }
+
+    }
+
 }
